aa2755d58c6d69ba6bc750d27e02eacb2c4049bf
Hanna Fuentes <fhannalucia@gmail.com>
2026-01-29 13:41:40 -0400

    primer avance del laboratorio

---
+++ laboratorio.cpp
@@ -0,0 +1,9 @@
+// hola_mundo.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "¡Hola Mundo desde Git!" << endl;
+    cout << "Mi nombre es: Hanna" << endl;
+    return 0;
+}
